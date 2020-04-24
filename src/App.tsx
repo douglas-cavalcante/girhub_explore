10aa0cf59a718f4f193dacb3d8ecd58c4d289a49
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2020-04-24 01:09:31 -0300

    Adiciona estilo global da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
